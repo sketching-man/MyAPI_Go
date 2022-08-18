d30afd8088c7c29866b1714cb7d83d5abd9a7793
Jaemin Lee <dlwoals238@gmail.com>
2022-08-18 22:35:12 +0900

    [Add] Domain struct 추가
    [Add] Domain struct 추가

---
+++ myboard/article.go
@@ -0,0 +1,9 @@
+package myboard
+
+type article struct {
+	Id          int64
+	Body        string
+	Title       string
+	WrittenTime string
+	WriterId    int64
+}
---
+++ myboard/member.go
@@ -0,0 +1,8 @@
+package myboard
+
+type member struct {
+	Id       int64
+	Grade    int
+	Password string
+	Username string
+}
