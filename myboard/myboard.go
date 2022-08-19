9dae039858ac0bb3b449408aa340497976228bc7
Jaemin Lee <dlwoals238@gmail.com>
2022-08-19 23:46:34 +0900

    [Edit] PUT, DELETE 함수 동작 구현 (Update, Delete)
    [Edit] PUT, DELETE 함수 동작 구현 (Update, Delete)

--- myboard/myboard.go
+++ myboard/myboard.go
@@ -89,11 +89,37 @@ func ReadMember(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateMember(w http.ResponseWriter, r *http.Request) {
+	params, ok := r.URL.Query()["id"]
+	if !ok || 1 > len(params[0]) {
+		return
+	}
 
+	var member member
+	err := json.NewDecoder(r.Body).Decode(&member)
+	if nil != err {
+		panic(err)
+	}
+
+	queryResult, err := db.Exec(
+		"UPDATE member SET grade=$1, password=$2, user_name=$3 WHERE id=$4;",
+		member.Grade, member.Password, member.Username, params[0])
+	if nil != err {
+		fmt.Println(queryResult)
+		panic(err)
+	}
 }
 
 func DeleteMember(w http.ResponseWriter, r *http.Request) {
+	params, ok := r.URL.Query()["id"]
+	if !ok || 1 > len(params[0]) {
+		return
+	}
 
+	queryResult, err := db.Exec("DELETE FROM member WHERE id=$1;", params[0])
+	if nil != err {
+		fmt.Println(queryResult)
+		panic(err)
+	}
 }
 
 func CreateArticle(w http.ResponseWriter, r *http.Request) {
@@ -146,9 +172,35 @@ func ReadArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateArticle(w http.ResponseWriter, r *http.Request) {
+	params, ok := r.URL.Query()["id"]
+	if !ok || 1 > len(params[0]) {
+		return
+	}
 
+	var article article
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if nil != err {
+		panic(err)
+	}
+
+	queryResult, err := db.Exec(
+		"UPDATE article SET body=$1, title=$2 WHERE id=$3;",
+		article.Body, article.Title, params[0])
+	if nil != err {
+		fmt.Println(queryResult)
+		panic(err)
+	}
 }
 
 func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+	params, ok := r.URL.Query()["id"]
+	if !ok || 1 > len(params[0]) {
+		return
+	}
 
+	queryResult, err := db.Exec("DELETE FROM article WHERE id=$1;", params[0])
+	if nil != err {
+		fmt.Println(queryResult)
+		panic(err)
+	}
 }
