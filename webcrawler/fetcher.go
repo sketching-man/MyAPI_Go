e50df54893e815e3e1abc165cd2c16651bf0c24f
Jaemin Lee <dlwoals238@gmail.com>
2022-08-28 23:46:51 +0900

    [Edit] hackerrank 메인 페이지의 링크 crawl 기능 작성 (테스트)
    [Edit] 테스트로 hackerrank 메인 페이지의 링크 crawl 기능 작성

--- webcrawler/fetcher.go
+++ webcrawler/fetcher.go
@@ -1,5 +1,34 @@
 package webcrawler
 
-func CrawlTest() {
+import (
+	"fmt"
+	"net/http"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+func CrawlTest() {
+	resp, err := http.Get("https://news.ycombinator.com/news")
+	if nil != err {
+		// do something
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if nil != err {
+		// do something
+	}
+
+	f := func(i int, s *goquery.Selection) bool {
+		link, _ := s.Attr("href")
+		return strings.HasPrefix(link, "https")
+	}
+
+	doc.Find("body a").FilterFunction(f).Each(
+		func(_ int, tag *goquery.Selection) {
+			link, _ := tag.Attr("href")
+			linkText := tag.Text()
+			fmt.Printf("%s %s\n", linkText, link)
+		})
 }
