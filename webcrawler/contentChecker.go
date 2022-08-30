ed49fc692b811c020c6780c29e8bce9e2e182609
Jaemin Lee <dlwoals238@gmail.com>
2022-08-31 01:14:35 +0900

    [Edit] HTML 해시 값 생성하여 중복 비교 시퀀스 추가
    [Edit] HTML 해시 값 생성하여 중복 비교 시퀀스 추가 (기본 옵션, 추후 알고리즘 개선?)

--- webcrawler/contentChecker.go
+++ webcrawler/contentChecker.go
@@ -1,12 +1,37 @@
 package webcrawler
 
-import "github.com/PuerkitoBio/goquery"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"hash/fnv"
+)
 
 func CheckDuplication(doc *goquery.Document) bool {
 	hash := getContentHash(doc)
-	return hash == ""
+
+	var exists bool
+	err := db.QueryRow(
+		"SELECT COUNT(*) FROM \"docRecord\" WHERE hash=$1",
+		hash).Scan(&exists)
+	if err != nil {
+		panic(err)
+	}
+
+	return exists
 }
 
-func getContentHash(doc *goquery.Document) string {
-	return ""
+func getContentHash(doc *goquery.Document) uint32 {
+	html, err := doc.Html()
+	if err != nil {
+		panic(err)
+	}
+
+	h := fnv.New32a()
+	length, err := h.Write([]byte(html))
+	if err != nil {
+		fmt.Println(length)
+		panic(err)
+	}
+
+	return h.Sum32()
 }
